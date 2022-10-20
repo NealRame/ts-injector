e079200b0fa213a472a5971a94d4242eb22093c2
Julien Graziano <julien@graziano.fr>
2022-10-21 00:09:42 +0200

    build: configure eslint

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -24,6 +24,7 @@ import {
     isService,
 } from "./utils"
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isNil(v: any): v is null | undefined {
     return v == null
 }
