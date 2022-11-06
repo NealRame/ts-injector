757582ebaff33d6689c30a4496ac6a9496340e0e
Julien Graziano <julien@graziano.fr>
2022-11-07 00:23:52 +0100

    build: update unit tests output

--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -2,4 +2,4 @@
 
 TS_NODE_COMPILER_OPTIONS='{"module": "commonjs"}' \
 TS_NODE_PROJECT="$PWD/src/tests/tsconfig.json" \
-    mocha -r ts-node/register "src/tests/**/*.ts"
+    mocha -r ts-node/register -R min "src/tests/**/*.ts"
--- src/tests/decorators.ts
+++ src/tests/decorators.ts
@@ -1,9 +1,67 @@
 import { expect } from "chai"
 
 import { Service } from "../lib/decorators"
+import {
+    ServiceLifecycle,
+} from "../lib/types"
+import {
+    getServiceMetadata,
+} from "../lib/utils"
 
 describe("@Service", () => {
-    it("return a decorator", () => {
+    it("is a decorator factory", () => {
+        expect(Service).to.be.a("function")
         expect(Service()).to.be.a("function")
+    })
+    it("adds metadata to the decorated class", () => {
+        @Service()
+        class ServiceClass {}
+        const metadata = getServiceMetadata(ServiceClass)
+        expect(metadata).to.be.an("object")
+        expect(metadata).to.have.property("lifecycle")
+        expect(metadata).to.have.property("parameters")
+    })
+    it("set lifecycle to be Transient by default", () => {
+        @Service()
+        class ServiceClass {}
+        const metadata = getServiceMetadata(ServiceClass)
+        expect(metadata.lifecycle).to.equal(ServiceLifecycle.Transient)
+    })
+    it("set lifecycle as expected", () => {
+        // Set lifecycle to be Transient
+        @Service({
+            lifecycle: ServiceLifecycle.Transient,
+        })
+        class ServiceClass1 {}
+        expect(getServiceMetadata(ServiceClass1).lifecycle).to.equal(ServiceLifecycle.Transient)
+        // Set lifecycle to be Singleton
+        @Service({
+            lifecycle: ServiceLifecycle.Singleton,
+        })
+        class ServiceClass2 {}
+        expect(getServiceMetadata(ServiceClass2).lifecycle).to.equal(ServiceLifecycle.Singleton)
+    })
+    it("set factoryFunction as expected", () => {
+        const factoryFunction = () => new ServiceClass
+        @Service({
+            factoryFunction,
+        })
+        class ServiceClass {}
+        const metadata = getServiceMetadata(ServiceClass)
+        expect(metadata.factoryFunction).to.equal(factoryFunction)
+    })
+    it("set factoryClass as expected", () => {
+        @Service()
+        class FactoryClass {
+            create() {
+                return new ServiceClass
+            }
+        }
+        @Service({
+            factoryClass: FactoryClass,
+        })
+        class ServiceClass {}
+        const metadata = getServiceMetadata(ServiceClass)
+        expect(metadata.factoryClass).to.equal(FactoryClass)
     })
 })
