cbb3c1a543d03b948c78f087a0af5fc05e863a10
Julien Graziano <julien@graziano.fr>
2022-11-07 01:10:09 +0100

    tests: add tests for Default decorator

---
+++ src/tests/decorators/default.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai"
+
+import {
+    getServiceMetadata,
+    Default,
+    Service,
+} from "../../lib/"
+
+describe("@Default", () => {
+    it("is a decorator factory", () => {
+        expect(Service).to.be.a("function")
+        expect(Service()).to.be.a("function")
+    })
+    it("adds metadata to the decorated class", () => {
+        class ServiceClass {
+            constructor(
+                @Default("test") public value: string,
+            ) {}
+        }
+        const metadata = getServiceMetadata(ServiceClass)
+        expect(metadata).to.have.property("parameters")
+        expect(metadata.parameters.get(0)).to.deep.equal({
+            fallback: "test",
+        })
+    })
+})
