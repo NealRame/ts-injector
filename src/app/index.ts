30af0be17f057a82945474b653f84b7d13c9ee71
Julien Graziano <julien@graziano.fr>
2022-12-01 00:22:31 +0100

    refactor: use .env file to config debug app

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,18 @@
+import * as path from "path"
+import * as dotenv from "dotenv"
+
 import app1 from "./app1"
 import app2 from "./app2"
+
+dotenv.config({
+    path: path.join(__dirname, "..", "..", ".env"),
+})
 
 const apps = {
     app1,
     app2,
 }
 
-;(process.env.APPS?.split(":") ?? Object.keys(apps)).forEach(app => {
+;(process.env.TEST_APPS?.split(":") ?? Object.keys(apps)).forEach(app => {
     apps[app as keyof typeof apps]?.()
 })
